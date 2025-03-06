7e4222ff366d660ed9df6e81b6ec2b6e45d0f63c
Arpit Agarwal <arpitagarwal2901@gmail.com>
2025-03-05 17:11:50 -0800

    Convert to regular Swift package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "ContactsManager",
-    version: "0.0.1",
     platforms: [
         .iOS(.v13)
     ],
@@ -12,14 +11,10 @@ let package = Package(
             name: "ContactsManager",
             targets: ["ContactsManager"])
     ],
-    dependencies: [
-        // Add any dependencies here
-    ],
     targets: [
         .binaryTarget(
             name: "ContactsManager",
-            url: "https://github.com/arpwal/contactsmanager-ios/releases/download/0.0.1/ContactsManager.xcframework.zip",
-            checksum: "5dab672b5a1f8ea55a5759dbb72e7f435cb59c4b9e705b35df71ce75aa336e2e"
+            path: "ContactsManager.xcframework"
         )
     ]
 )
