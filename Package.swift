7fb813f6e50bcecd668095325828cc5c8ad661b4
Arpit Agarwal <arpitagarwal2901@gmail.com>
2025-05-08 15:17:39 -0700

    Update Swift tools version to 5.9 and specify iOS platform version 17 in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,23 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "ContactsManager",
-    products: [
-        .library(
-            name: "ContactsManager",
-            targets: ["ContactsManager"])
-    ],
-    dependencies: [
-        // No dependencies
-    ],
-    targets: [
-        .binaryTarget(
-            name: "ContactsManager",
-            path: "ContactsManager.xcframework"
-        )
-    ]
+  name: "ContactsManager",
+  platforms: [
+    .iOS(.v17)
+  ],
+  products: [
+    .library(
+      name: "ContactsManager",
+      targets: ["ContactsManager"])
+  ],
+  dependencies: [
+    // No dependencies
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ContactsManager",
+      path: "ContactsManager.xcframework"
+    )
+  ]
 )
